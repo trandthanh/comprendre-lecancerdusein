40c1baaf74217aa8c97db287e6b265c609d97776
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-09-25 16:54:13 +0200

    Finished Maladie Index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,9 @@
 class QuestionsController < ApplicationController
 
+  def index
+    @questions = Question.all
+  end
+
   def show
     @question_url = params[:url]
     @question = Question.find_by(url: @question_url)
